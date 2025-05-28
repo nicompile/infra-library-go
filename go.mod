3999f33cc6cfd68fb86973dfa9d0222741d9bb9e
nicompile <nicompile@tailwindmail.com>
2025-05-27 23:29:35 -0700

    aws helper methods

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/nicompile/infra-library-go
 
 go 1.22.2
 
-require github.com/go-yaml/yaml v2.1.0+incompatible
+require (
+	github.com/aws/aws-lambda-go v1.48.0
+	github.com/go-yaml/yaml v2.1.0+incompatible
+)
 
 require (
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
--- pkg/serverlessapi/serverlessapi.go
+++ pkg/serverlessapi/serverlessapi.go
@@ -1,5 +1,11 @@
 package serverlessapi
 
+import (
+	"context"
+
+	"github.com/aws/aws-lambda-go/events"
+)
+
 type Endpoint struct {
 	Method  string
 	Path    string
@@ -26,4 +32,32 @@ type Response struct {
 	Headers           map[string]string   `json:"headers"`
 	MultiValueHeaders map[string][]string `json:"multiValueHeaders"`
 	Body              string              `json:"body"`
+}
+
+func BuildHandler(endpointMethod func(Request) Response) func(context.Context, events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
+	return func(ctx context.Context, event events.APIGatewayProxyRequest) events.APIGatewayProxyResponse {
+		req := requestFromAwsModel(event)
+		resp := endpointMethod(req)
+		return resp.toAwsModel()
+	}
+}
+
+func requestFromAwsModel(event events.APIGatewayProxyRequest) Request {
+	return Request{
+		Headers:                         event.Headers,
+		MultiValueHeaders:               event.MultiValueHeaders,
+		QueryStringParameters:           event.QueryStringParameters,
+		MultiValueQueryStringParameters: event.MultiValueQueryStringParameters,
+		PathParameters:                  event.PathParameters,
+		Body:                            event.Body,
+	}
+}
+
+func (r Response) toAwsModel() events.APIGatewayProxyResponse {
+	return events.APIGatewayProxyResponse{
+		StatusCode:        r.StatusCode,
+		Headers:           r.Headers,
+		MultiValueHeaders: r.MultiValueHeaders,
+		Body:              r.Body,
+	}
 }
